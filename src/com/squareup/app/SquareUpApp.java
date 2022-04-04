406a92e64abff02d0af231b14183614fd8fa5ef7
Mannie004 <seun.adeyemi04@yahoo.com>
2022-04-04 12:20:04 -0400

    Update from main class and squareUp app

--- src/com/squareup/app/SquareUpApp.java
+++ src/com/squareup/app/SquareUpApp.java
@@ -1,5 +1,31 @@
 package com.squareup.app;
 
-class SquareUpApp {
+public class SquareUpApp {
 
+    private String playerX ;
+    private String playerO;
+
+    //public Main(String playerX, String playerO) {
+       // this.playerX = playerX;
+        //this.playerO = playerO;
+   // }
+
+    // Methods
+    public void tie() {
+    }
+    public void win() {
+    }
+    public void execute() {
+    }
+
+    // Accessors
+    public  void  makeMove() {
+    }
+    public String getPlayerX() {
+        return playerX;
+    }
+    public String getPlayerO() {
+        return playerO;
+
+    }
 }
--- src/com/squareup/client/Main.java
+++ src/com/squareup/client/Main.java
@@ -1,5 +1,11 @@
 package com.squareup.client;
 
+import com.squareup.app.SquareUpApp;
+
 class Main {
 
-}
+        public static void main(String[] args) {
+            SquareUpApp app= new SquareUpApp();
+            app.execute();
+        }
+    }
