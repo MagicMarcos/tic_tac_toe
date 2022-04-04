35f2b95b3947cd741cce3850a48c7bf6b3e8a7ab
Mannie004 <seun.adeyemi04@yahoo.com>
2022-04-04 16:35:12 -0400

    Update from main class and squareUp app

--- src/com/squareup/app/SquareUpApp.java
+++ src/com/squareup/app/SquareUpApp.java
@@ -1,16 +1,25 @@
 package com.squareup.app;
 
 import com.squareup.Board;
+
 import java.util.Scanner;
 
 public class SquareUpApp {
 
     private final Board board = Board.getInstance();
+    private Scanner input = new Scanner(System.in);
 
+
+    // methods
     public void execute() {
         board.printGameBoard();
+        welcome();
     }
-    // Create user input interface from console
-    Scanner input = new Scanner(System.in);
 
+    private void welcome() {
+        System.out.println("\n");
+        System.out.println("W E L C O M E  T O  T H E  S Q U A R E U P  A P P L I C A T I O N");
+        System.out.println("\n\n");
+
+    }
 }
