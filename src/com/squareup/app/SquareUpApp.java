9c7a85f7c231a7c6f57ebf275a1c4e918f4e6163
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2022-04-04 16:31:07 -0400

    square up app execute method

--- src/com/squareup/app/SquareUpApp.java
+++ src/com/squareup/app/SquareUpApp.java
@@ -8,9 +8,18 @@ public class SquareUpApp {
     private final Board board = Board.getInstance();
 
     public void execute() {
+        // welcome banner
+        //show board
+        showBoard();
+        // nextTurn
+        //showboard
+        showBoard();
+    }
+
+    private void showBoard(){
         board.printGameBoard();
+
     }
     // Create user input interface from console
     Scanner input = new Scanner(System.in);
-
 }
