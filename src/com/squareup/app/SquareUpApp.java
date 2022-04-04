0988436c027cf1c20e234e7da7523eacd4384f4b
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2022-04-04 16:49:11 -0400

    add a welcome banner

--- src/com/squareup/app/SquareUpApp.java
+++ src/com/squareup/app/SquareUpApp.java
@@ -9,15 +9,23 @@ public class SquareUpApp {
     private Scanner input = new Scanner(System.in);
 
     public void execute() {
+        // welcome banner
         welcome();
-        board.printGameBoard();
+        //show board
+        showBoard();
+        // nextTurn
+        //showboard
+        showBoard();
     }
 
+    private void showBoard(){
+        board.printGameBoard();
+
+    }
 
     private void welcome() {
         System.out.println("\n");
-        System.out.println("W E L C O M E  T O  T H E  S Q U A R E U P  A P P L I C A T I O N");
+        System.out.println("W E L C O M E T O T H E S Q U A R E U P A P P L I C A T I O N");
         System.out.println("\n\n");
-
     }
 }
