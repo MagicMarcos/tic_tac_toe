ac4d82224bfc7fae1fb340bc68675ed7bfcbeea0
Justin <justin.bruce462@gmail.com>
2022-04-04 15:15:17 -0400

    added scanner to SquareUpApp

--- src/com/squareup/app/SquareUpApp.java
+++ src/com/squareup/app/SquareUpApp.java
@@ -1,11 +1,20 @@
 package com.squareup.app;
 
 import com.squareup.Board;
+import java.util.Scanner;
 
 public class SquareUpApp {
+
     private final Board board = Board.getInstance();
+
     public void execute() {
         board.printGameBoard();
+    }
+    // Create user input interface from console
+    Scanner input = new Scanner(System.in);
+    for(int i=0; i<9 ; i++){
+        board[i] = String.valueOf(i+1);
+
     }
 
 }
